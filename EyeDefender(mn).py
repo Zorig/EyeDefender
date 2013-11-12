9b8bb20881e813b739adfc20f4a43d0007cf4804
zorig ganbold <zorig.ganbold@gmail.com>
2013-11-13 02:20:37 +0800

    english and mongolian version also readme added

---
+++ EyeDefender(mn).py
@@ -0,0 +1,18 @@
+#coding:utf-8
+import time
+import pynotify
+import os
+def message(message):
+	pynotify.init("EyeDefender")
+	notice=pynotify.Notification(message)
+	notice.show()
+	return
+
+s=0
+while s<=3600:
+   print s,'sec' 
+   time.sleep(1)
+   s+=1
+   if s==3600:
+   	message(u'Нүдээ амраах цаг боллоо!')
+   	s=0
